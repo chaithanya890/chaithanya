9f551938750b2886eaeaba5bfeaf5bba4b5eb665
chaithanya890 <chaithanyasucksatlife@gmail.com>
2025-01-27 09:49:36 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+package world;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		for(int r=5;r>=1;r--) {
+			for(int c=5;c>=r;c--) {
+				System.out.print(c+" ");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
---
+++ Pattern2.java
@@ -0,0 +1,14 @@
+package world;
+
+public class Pattern2 {
+	public static void main(String[] args) {
+		for(int r=1;r<=5;r++) {
+			        for(int c=5;c>=r;c--) {
+			        	System.out.print(c+" ");
+			        }
+			        System.out.println();
+			  
+		}
+	}
+
+}
