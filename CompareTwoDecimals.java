9baf1d19865cd8ab8e3a3bf715e1178f907414ed
chaithanya890 <chaithanyasucksatlife@gmail.com>
2025-01-27 09:52:04 +0530

    Add files via upload

---
+++ CompareTwoDecimals.java
@@ -0,0 +1,13 @@
+
+
+public class CompareTwoDecimals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//write a program to input any character and check
+		//whether it is alphabet,
+		//digit or special character
+	}
+
+}
